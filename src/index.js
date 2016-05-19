a52eca734cce4a143c23bb4ed841b42eb99652db
martynov <oleg.martynov@ligastavok.ru>
2016-05-19 19:30:20 +0300

    add support for preload promises

--- src/index.js
+++ src/index.js
@@ -1,35 +1,70 @@
 import React from 'react';
 
 class FetcherContext extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            canTransition: true
+        };
+    }
     componentWillMount() {
-        fetchData(this.props, item => item.deferred);
+        fetchData({ props: this.props, filter: item => item.deferred, fetcherContext: this });
     }
     componentWillReceiveProps(nextProps) {
-        fetchData(nextProps);
+        fetchData({ props: nextProps, fetcherContext: this });
     }
-    shouldComponentUpdate() {
-        // TODO: block transition for some promises
-        return true;
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextState.canTransition;
     }
     render() {
         return this.props.children;
     }
 }
 
-function fetchData(props, filter) {
+function fetchData({ props, filter, fetcherContext }) {
     let promises = [];
+    let preloadPromises = [];
+    let rawPromises = [];
+
+    // get promises from components in flat array
     props.components.forEach(component => {
         if (Array.isArray(component.promises)) {
             promises = promises.concat(component.promises);
         }
     });
+
+    // filter deferred items
     if (filter) promises = promises.filter(filter);
-    promises = promises.map(promise => promise.promise(props));
-    return Promise.all(promises).catch(error => console.error('react-router-fetcher error', error));
+
+    // prepare for execution
+    promises.forEach(promise => {
+        if (promise.preload) {
+            preloadPromises.push(promise.promise(props))
+        } else {
+            rawPromises.push(promise.promise(props))
+        }
+    });
+    if (fetcherContext && preloadPromises.length) {
+        fetcherContext.setState({
+            canTransition: false
+        });
+    }
+
+    // execute promises and return result
+    rawPromises.push(
+        Promise.all(preloadPromises).then(() => {
+            fetcherContext && fetcherContext.setState({
+                canTransition: true
+            });
+        })
+    );
+    return Promise.all(rawPromises).catch(error => console.error('react-router-fetcher error', error));
+    // promises = promises.map(promise => promise.promise(props));
+    // return Promise.all(promises).catch(error => console.error('react-router-fetcher error', error));
 }
 
 export function fetchOnServer(props) {
-    return fetchData(props, item => !item.deferred);
+    return fetchData({ props, filter: item => !item.deferred });
 }
 
 export function fetcher(items) {
