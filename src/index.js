1d4240398f6da488db2223a83ea4a900309c4e0d
martynov <oleg.martynov@ligastavok.ru>
2016-05-19 12:33:28 +0300

    add support for deffered promises

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
 import React from 'react';
 
 class FetcherContext extends React.Component {
+    componentWillMount() {
+        fetchData(this.props, item => item.deferred);
+    }
     componentWillReceiveProps(nextProps) {
         fetchData(nextProps);
+    }
+    shouldComponentUpdate() {
+        // TODO: block transition for some promises
+        return true;
     }
     render() {
         return this.props.children;
     }
 }
 
-export function fetchData(props) {
+function fetchData(props, filter) {
     let promises = [];
     props.components.forEach(component => {
         if (Array.isArray(component.promises)) {
             promises = promises.concat(component.promises);
         }
     });
+    if (filter) promises = promises.filter(filter);
     promises = promises.map(promise => promise.promise(props));
-    // TODO: catch for govnars
-    return Promise.all(promises);
+    return Promise.all(promises).catch(error => console.error('react-router-fetcher error', error));
+}
+
+export function fetchOnServer(props) {
+    return fetchData(props, item => !item.deferred);
 }
 
 export function fetcher(items) {
